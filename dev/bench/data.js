7e697798d10977ceb53db2e47e04084a92d1c44d
github-action-benchmark <github@users.noreply.github.com>
2022-12-27 16:53:02 +0000

    add Benchmark (benchmarkjs) benchmark result for 8e71c1425e16160ea8edcee18d1db4fa6f946ff3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1672148645986,
+  "lastUpdate": 1672159982965,
   "repoUrl": "https://github.com/enell/node-red-contrib-genetic-charging-scheduler",
   "entries": {
     "Benchmark.js Benchmark": [
@@ -29,6 +29,39 @@ window.BENCHMARK_DATA = {
             "name": "calculate schedule",
             "value": 4.82,
             "range": "±1.32%",
+            "unit": "ops/sec",
+            "extra": "16 samples"
+          }
+        ]
+      }
+    ],
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "johan.enell@qlik.com",
+            "name": "Johan Enell",
+            "username": "enell"
+          },
+          "committer": {
+            "email": "johan.enell@qlik.com",
+            "name": "Johan Enell",
+            "username": "enell"
+          },
+          "distinct": true,
+          "id": "8e71c1425e16160ea8edcee18d1db4fa6f946ff3",
+          "message": "ci: fixed master benchmark",
+          "timestamp": "2022-12-27T17:52:17+01:00",
+          "tree_id": "5399ec43a53fd201a6e5b20846323551a67f8268",
+          "url": "https://github.com/enell/node-red-contrib-genetic-charging-scheduler/commit/8e71c1425e16160ea8edcee18d1db4fa6f946ff3"
+        },
+        "date": 1672159980426,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "calculate schedule",
+            "value": 4.58,
+            "range": "±3.46%",
             "unit": "ops/sec",
             "extra": "16 samples"
           }
