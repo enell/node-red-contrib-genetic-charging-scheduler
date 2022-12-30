ce0673dcadda476fd8e45a39e97c2dca209c98b7
github-action-benchmark <github@users.noreply.github.com>
2022-12-30 08:31:27 +0000

    add Calculate (benchmarkjs) benchmark result for 245d8d06c7276f0cf97bc322aa1a83d201817788

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,102 +1,39 @@
 window.BENCHMARK_DATA = {
-  lastUpdate: 1672160457361,
-  repoUrl:
-    'https://github.com/enell/node-red-contrib-genetic-charging-scheduler',
-  entries: {
-    Calculate: [
+  "lastUpdate": 1672389087384,
+  "repoUrl": "https://github.com/enell/node-red-contrib-genetic-charging-scheduler",
+  "entries": {
+    "Calculate": [
       {
-        commit: {
-          author: {
-            email: 'johan.enell@qlik.com',
-            name: 'Johan Enell',
-            username: 'enell',
+        "commit": {
+          "author": {
+            "email": "johan.enell@qlik.com",
+            "name": "Johan Enell",
+            "username": "enell"
           },
-          committer: {
-            email: 'johan.enell@qlik.com',
-            name: 'Johan Enell',
-            username: 'enell',
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
           },
-          distinct: true,
-          id: 'dc5e7a349cbf658ff5eec3b56d2ff166980a5ae4',
-          message: 'ci: added name to action',
-          timestamp: '2022-12-27T18:00:14+01:00',
-          tree_id: 'd3dd55c2d58f546b22a3b6900283ef651a6f9539',
-          url: 'https://github.com/enell/node-red-contrib-genetic-charging-scheduler/commit/dc5e7a349cbf658ff5eec3b56d2ff166980a5ae4',
+          "distinct": true,
+          "id": "245d8d06c7276f0cf97bc322aa1a83d201817788",
+          "message": "feat: New fitness",
+          "timestamp": "2022-12-30T09:30:47+01:00",
+          "tree_id": "c892ffc520ece3342923f2c95cd034202a8d97ba",
+          "url": "https://github.com/enell/node-red-contrib-genetic-charging-scheduler/commit/245d8d06c7276f0cf97bc322aa1a83d201817788"
         },
-        date: 1672160455423,
-        tool: 'benchmarkjs',
-        benches: [
+        "date": 1672389085713,
+        "tool": "benchmarkjs",
+        "benches": [
           {
-            name: 'calculate schedule',
-            value: 4.65,
-            range: '±3.79%',
-            unit: 'ops/sec',
-            extra: '16 samples',
-          },
-        ],
-      },
-      {
-        commit: {
-          author: {
-            email: 'johan.enell@qlik.com',
-            name: 'Johan Enell',
-            username: 'enell',
-          },
-          committer: {
-            email: 'johan.enell@qlik.com',
-            name: 'Johan Enell',
-            username: 'enell',
-          },
-          distinct: true,
-          id: '8e71c1425e16160ea8edcee18d1db4fa6f946ff3',
-          message: 'ci: fixed master benchmark',
-          timestamp: '2022-12-27T17:52:17+01:00',
-          tree_id: '5399ec43a53fd201a6e5b20846323551a67f8268',
-          url: 'https://github.com/enell/node-red-contrib-genetic-charging-scheduler/commit/8e71c1425e16160ea8edcee18d1db4fa6f946ff3',
-        },
-        date: 1672159980426,
-        tool: 'benchmarkjs',
-        benches: [
-          {
-            name: 'calculate schedule',
-            value: 4.58,
-            range: '±3.46%',
-            unit: 'ops/sec',
-            extra: '16 samples',
-          },
-        ],
-      },
-      {
-        commit: {
-          author: {
-            email: 'johan.enell@qlik.com',
-            name: 'Johan Enell',
-            username: 'enell',
-          },
-          committer: {
-            email: 'johan.enell@qlik.com',
-            name: 'Johan Enell',
-            username: 'enell',
-          },
-          distinct: true,
-          id: '7bd47eed5e830aec3d4c423d936102e524ace042',
-          message: 'ci: added benchmark for calculating schedule',
-          timestamp: '2022-12-27T14:36:18+01:00',
-          tree_id: '1eac7d8445ff2951678503d6aa2b16c7b2b0329a',
-          url: 'https://github.com/enell/node-red-contrib-genetic-charging-scheduler/commit/7bd47eed5e830aec3d4c423d936102e524ace042',
-        },
-        date: 1672148644290,
-        tool: 'benchmarkjs',
-        benches: [
-          {
-            name: 'calculate schedule',
-            value: 4.82,
-            range: '±1.32%',
-            unit: 'ops/sec',
-            extra: '16 samples',
-          },
-        ],
-      },
-    ],
-  },
+            "name": "calculate schedule",
+            "value": 3.2,
+            "range": "±2.42%",
+            "unit": "ops/sec",
+            "extra": "13 samples"
+          }
+        ]
+      }
+    ]
+  }
 }
