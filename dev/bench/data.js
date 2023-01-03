d53cc72fc4c434db24c02eae548bbef06af19ebd
github-action-benchmark <github@users.noreply.github.com>
2023-01-03 09:23:36 +0000

    add Calculate (benchmarkjs) benchmark result for 5e7d04037df2f6bbeb24226b4d9a041ed60c1fbd

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1672389087384,
+  "lastUpdate": 1672737816243,
   "repoUrl": "https://github.com/enell/node-red-contrib-genetic-charging-scheduler",
   "entries": {
     "Calculate": [
@@ -29,6 +29,37 @@ window.BENCHMARK_DATA = {
             "name": "calculate schedule",
             "value": 3.2,
             "range": "±2.42%",
+            "unit": "ops/sec",
+            "extra": "13 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "johan.enell@qlik.com",
+            "name": "Johan Enell",
+            "username": "enell"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5e7d04037df2f6bbeb24226b4d9a041ed60c1fbd",
+          "message": "Merge pull request #9 from enell/alpha\n\nfeat: read initial battery soc from payload",
+          "timestamp": "2023-01-03T10:22:56+01:00",
+          "tree_id": "d32eb05c2a6c79c0335520eca7e22222e982cb99",
+          "url": "https://github.com/enell/node-red-contrib-genetic-charging-scheduler/commit/5e7d04037df2f6bbeb24226b4d9a041ed60c1fbd"
+        },
+        "date": 1672737814307,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "calculate schedule",
+            "value": 3.21,
+            "range": "±1.72%",
             "unit": "ops/sec",
             "extra": "13 samples"
           }
