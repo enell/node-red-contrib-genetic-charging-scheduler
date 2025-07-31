0472f87cadc28b05c1a8586d233d082357680522
github-action-benchmark <github@users.noreply.github.com>
2025-07-31 21:49:54 +0000

    add Calculate (benchmarkjs) benchmark result for 271b5032bfc91e88c42195d3e7223193a8357986

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1753998589219,
+  "lastUpdate": 1753998594117,
   "repoUrl": "https://github.com/enell/node-red-contrib-genetic-charging-scheduler",
   "entries": {
     "Calculate": [
@@ -7626,6 +7626,37 @@ window.BENCHMARK_DATA = {
             "range": "±0.55%",
             "unit": "ops/sec",
             "extra": "90 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "271b5032bfc91e88c42195d3e7223193a8357986",
+          "message": "chore(deps): update dependency typescript to v5.9.2 (#394)\n\nCo-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>",
+          "timestamp": "2025-07-31T21:49:24Z",
+          "tree_id": "96d38685e99a500bdf1767fa4636c517427b823e",
+          "url": "https://github.com/enell/node-red-contrib-genetic-charging-scheduler/commit/271b5032bfc91e88c42195d3e7223193a8357986"
+        },
+        "date": 1753998592229,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "calculate schedule",
+            "value": 1206468,
+            "range": "±0.42%",
+            "unit": "ops/sec",
+            "extra": "93 samples"
           }
         ]
       }
