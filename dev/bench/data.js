06ec89a4702bb9d4b7be7d68ee6a2c52356f2b2a
github-action-benchmark <github@users.noreply.github.com>
2025-11-23 14:02:21 +0000

    add Calculate (benchmarkjs) benchmark result for ce2a5fd5b829919402a5ad4c21303b0b7cabdc74

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1763749705257,
+  "lastUpdate": 1763906541354,
   "repoUrl": "https://github.com/enell/node-red-contrib-genetic-charging-scheduler",
   "entries": {
     "Calculate": [
@@ -9734,6 +9734,37 @@ window.BENCHMARK_DATA = {
             "range": "±0.40%",
             "unit": "ops/sec",
             "extra": "92 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ce2a5fd5b829919402a5ad4c21303b0b7cabdc74",
+          "message": "chore(deps): update yarn to v4.12.0 (#462)\n\nCo-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>",
+          "timestamp": "2025-11-23T14:01:53Z",
+          "tree_id": "1d6885571313d02151c77c6167a27843435a48b3",
+          "url": "https://github.com/enell/node-red-contrib-genetic-charging-scheduler/commit/ce2a5fd5b829919402a5ad4c21303b0b7cabdc74"
+        },
+        "date": 1763906539599,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "calculate schedule",
+            "value": 1244803,
+            "range": "±1.29%",
+            "unit": "ops/sec",
+            "extra": "93 samples"
           }
         ]
       }
